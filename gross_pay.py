3740328101b57d56ebc88449a4233886dd54f242
Daniel2542 <77247207+Daniel2542@users.noreply.github.com>
2023-01-23 18:26:24 +0300

    Add files via upload

---
+++ gross_pay.py
@@ -0,0 +1,4 @@
+hours_worked=int(input("Enter number of hours worked"))
+rate_per_hour=float(input("Enter the rate per hr"))
+gross_pay=hours_worked*rate_per_hour
+print("Your gross pay is",gross_pay)
