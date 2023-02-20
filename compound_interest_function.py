8f53d53feb4821df94c77622cb1eacfed9dd8b49
Daniel2542 <77247207+Daniel2542@users.noreply.github.com>
2023-02-20 17:01:10 +0300

    Update compound_interest_function.py

--- compound_interest_function.py
+++ compound_interest_function.py
@@ -3,6 +3,7 @@
 def compound_interest(principal, rate, time):
     Amount = principal * (pow((1 + rate / 100), time))
     C_interest = Amount - principal
+    return C_interest
 principal=int(input("Enter principal amount:"))
 rate=int(input("Enter rate:"))
 time=int(input("Enter time:"))
