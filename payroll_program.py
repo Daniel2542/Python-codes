78a9c83928fbd86bf8ee6a2ecbd5db5d8970bb19
Daniel2542 <77247207+Daniel2542@users.noreply.github.com>
2023-03-13 16:03:55 +0300

    Add files via upload

---
+++ payroll_program.py
@@ -0,0 +1,13 @@
+#code to connect the interface with the payroll code
+
+import payroll_system_inheritance
+
+salary_employee = payroll_system_inheritance.Salaried_Employee(1, 'Daniel Wainaina', 20000)
+hourly_employee = payroll_system_inheritance.Hourly_Employee(2, 'Martin Njue', 1000, 15)
+commission_employee = payroll_system_inheritance.Commission_Employee(3, 'Danny', 10000, 250)
+payroll_system = payroll_system_inheritance.Payroll_System()
+payroll_system.calculate_payroll([
+    salary_employee,
+    hourly_employee,
+    commission_employee
+])    
